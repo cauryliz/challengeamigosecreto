799d53b25ebeec26c0f27b46605404d0bc40f27c
Claudia Sanchez <gotica166@gmail.com>
2025-03-13 12:26:31 -0600

    Agregue function agregar amigo y comence con la funcion que muestra la lista de amigos agregados

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById('amigo').value;
+    if(nombreAmigo === ''){
+alert('Por favor, ingresa un nombre');
+    } else {
+        listaDeAmigos.push(nombreAmigo);
+        document.querySelector('#amigo').value = '';
+        //mostrarListaAmigo();
+    }
+
+}
+//function agregarAmigo() {
+    //let nombreAmigo = document.getElementById("amigo").value;
+  
+    //if (nombreAmigo.trim() === "") {
+     // alert("Por favor, inserte un nombre");
+   // } else {
+    //  amigos.push(nombreAmigo);
+    //  document.querySelector("#amigo").value = "";
+    //  mostrarListaAmigo();
+   // }
